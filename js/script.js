be38bf56e35cda4d3a2ab7b1c7f245d7f6fea237
BinyamMamo <binyammamo01@gmail.com>
2022-11-23 12:10:33 +0300

    finished.

--- js/script.js
+++ js/script.js
@@ -6,17 +6,17 @@ function next(index){
     // var cont_two = document.getElementById("container-two");
 
     if(index == 1){
-        container.style.setProperty("--locus", "8rem");
-        before.style.borderTopLeftRadius = "0rem";
-        before.style.borderTopRightRadius = "0rem";
+        // container.style.setProperty("--locus", "7.5rem");
+        before.style.width = "17rem";
+        before.style.clipPath = "polygon(0% 0%, 85% 0%, 100% 50%, 85% 100%, 0% 100%)";
         cont_one.style.marginLeft = "-25rem";
         cont_one.style.transition = "0.75s";
     }
 
     if(index == 2){
-        container.style.setProperty("--locus", "16rem");
-        before.style.borderTopLeftRadius = "0rem";
+        before.style.width = "24rem";
         before.style.borderTopRightRadius = "0.4rem";
+        before.style.clipPath = "none";
         cont_one.style.marginLeft = "-50rem";
         cont_one.style.transition = "0.75s";
     }
@@ -30,17 +30,16 @@ function back(index){
     // var cont_two = document.getElementById("container-two");
 
     if(index == 1){
-        container.style.setProperty("--locus", "0rem");
-        before.style.borderTopLeftRadius = "0.4rem";
+        before.style.width = "9rem";
         before.style.borderTopRightRadius = "0rem";
+        before.style.clipPath = "polygon(0% 0%, 75% 0%, 100% 50%, 75% 100%, 0% 100%)";
         cont_one.style.marginLeft = "0rem";
         cont_one.style.transition = "0.75s";   
     }
 
     if(index == 2){
-        container.style.setProperty("--locus", "8rem");
-        before.style.borderTopLeftRadius = "0rem";
-        before.style.borderTopRightRadius = "0rem";
+        before.style.width = "17rem";
+        before.style.clipPath = "polygon(0% 0%, 85% 0%, 100% 50%, 85% 100%, 0% 100%)";
         cont_one.style.marginLeft = "-25rem";
         cont_one.style.transition = "0.75s";   
     }
